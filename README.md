fe74a7fca48e7b5efd9b25bb32d155561d8a3e8a
Marcos de Gois Borges <goisborges@gmail.com>
2022-06-02 22:24:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment-01
+ COMP2068 - Assignment 021
---
+++ routes/contact.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET about page. */
+//router are already registered to a specific route
+//intention is to register this object to '/about' route
+router.get('/', function(req, res, next) {
+  // tell express to render an hbs view template
+  res.render('contact', { title: 'Contact', email: 'goisborges@gmail.com' });
+})
+
+module.exports = router;
